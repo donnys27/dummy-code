7ced23eedc178c4fdae498464ed53fc5e71e5bfe
donnys27 <donisutopo@gmail.com>
2024-03-14 11:27:38 +0700

    commit

---
+++ code/configuration.php
@@ -0,0 +1,18 @@
+<?php
+$license = 'nulled_key';
+$db_host = 'localhost';
+$db_port = '';
+$db_username = 'usertest';
+$db_password = 'test1234';
+$db_name = 'dbtest';
+$templates_compiledir = 'templates_c';
+$mysql_charset = 'utf8';
+$customadminpath = 'whmcspriv';
+
+// Digunakan sebagai BCA Payment, Kode Unik
+define('MAX_CORRECTION', 3000);
+define('INV_KODEUNIK', FALSE);
+define('CI_API','https://localhost/localapi/');
+
+
+
